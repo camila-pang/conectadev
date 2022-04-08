a8b59001e943b4f086bb578b9c1648a906181c3a
camila-pang <camila_pang@hotmail.com>
2022-04-08 14:19:37 -0300

    alteracao

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,18 +4,7 @@ import style from './style.css'
 function Home() {
     return (
         <div>
-            <header className="header">
-                <div className="toolbar">
-                    <div>
-                        <span>Conecta DEV</span>
-                    </div>
-                    <div>
-                        <button>Novo Post</button>
-                        <span>img1</span>
-                        <span>img2</span>
-                    </div>
-                </div>
-            </header>
+           
             <main className='main'>
                 <div className='navbar'>navbar</div>
                 <div className='feed'>feed</div>
